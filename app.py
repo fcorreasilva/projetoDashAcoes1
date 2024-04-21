93151dbe517d820f0e29c265f7d17cdfd26a289a
Fabio <fcorreasilva1977@gmailcom>
2024-04-21 10:05:12 -0300

    nova correcao

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, redirect
-import mysql.connector
 import os
 import psycopg2
 
 app = Flask(__name__)
 
 # Obtenha as credenciais de conexão do ambiente
-DATABASE_URL = os.environ['postgres://vjqkijusfokyku:6ae5592a61d4da0a84f24098190a04a0c7f5b6a04f474a42b27e5a0e5b7be675@ec2-52-54-200-216.compute-1.amazonaws.com:5432/d9dlbkttuqamvs']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 # Conectando ao banco de dados
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -21,7 +20,7 @@ def index():
 # Rota para submeter o formulário
 @app.route("/submit", methods=["POST"])
 def submit():
-     if request.method == "POST":
+    if request.method == "POST":
         ticker = request.form["ticker"]
         nome = request.form["nome"]
 
@@ -32,6 +31,10 @@ def submit():
         conn.commit()
 
         return redirect("/")
+
+    # Caso a solicitação não seja POST, você pode lidar com isso aqui se necessário
+    return "Method Not Allowed", 405
+
 
 if __name__ == "__main__":
     app.run(debug=True)
